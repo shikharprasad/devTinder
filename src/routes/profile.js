6b3bc8fad50a8578667fb0dccc86f9897387de34
shikharprasad <shkp2002@gmail.com>
2025-04-23 21:58:28 +0530

    logout ?+ edit profile API

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const profileRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
+const {validateEditProfileData} = require("../utils/validation");
 
 
-
-profileRouter.get("/profile", userAuth, async (req, res) => {
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
     try {
         const user = req.user;
         res.send(user.toObject()); // Send plain object to avoid circular structure
@@ -13,5 +13,25 @@ profileRouter.get("/profile", userAuth, async (req, res) => {
     }
 });
 
+profileRouter.patch("/profile/edit", userAuth, async (req, res) =>{
+
+    try {
+        if(!validateEditProfileData(req)) {
+            throw new Error("Invalid Edit Request");
+        }
+
+        const loggedInuser = req.user;
+        console.log(loggedInuser);
+        Object.keys(req.body).forEach((key)=> (loggedInuser[key] = req.body[key]));
+        await loggedInuser.save();
+        console.log(loggedInuser);
+
+        res.json({message:`${loggedInuser.firstName},your profile updated successfully`, data: loggedInuser});
+        
+    } catch (err) {
+        res.status(400).send("ERROR : " + err.message);
+    }
+
+});
 
 module.exports = profileRouter;
